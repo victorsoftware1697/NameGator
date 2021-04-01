00dd9adfa159be0b2a58af90dff8c0efa43a580b
Victor Nascimento <victorsoftware1697@gmail.com>
2021-04-01 18:34:39 -0300

    initial commit

---
+++ server/database.js
@@ -0,0 +1,24 @@
+const {Client} = require("pg");
+
+const client = new Client({
+    user: "postgres",
+    password: "Highlife1697@",
+    host: "localhost",
+    database: "namegator",
+    port: 5432
+});
+
+client.connect();
+
+exports.execute = function (query, values) {
+    return new Promise(function (resolve, reject){
+        client.query(query, values, function (err, res) {
+            if (err) {
+                reject(err);
+            }else{
+                resolve(res.rows);
+            }
+        });
+    });
+};
+
